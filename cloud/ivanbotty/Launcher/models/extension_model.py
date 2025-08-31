22b22accce31ee3d399aa100121210f47b2cab31
BottyIvan <droidbotty@gmail.com>
2025-08-31 22:49:24 +0200

    Add ExtensionModel class for managing extension properties

---
+++ cloud/ivanbotty/Launcher/models/extension_model.py
@@ -0,0 +1,19 @@
+import gi
+gi.require_version("Gtk", "4.0")
+from gi.repository import GObject
+
+class ExtensionModel(GObject.GObject):
+    name = GObject.Property(type=str)
+    description = GObject.Property(type=str, default=None)
+    enabled = GObject.Property(type=bool, default=True)
+    version = GObject.Property(type=str, default=None)
+    author = GObject.Property(type=str, default=None)
+
+    def __init__(self, name, description=None, enabled=True, service=None, version=None, author=None):
+        super().__init__()
+        self.name = name
+        self.description = description
+        self.enabled = enabled
+        self.version = version
+        self.author = author
+        self.service = service
