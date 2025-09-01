c0d7eb4e077f53da8fedac85b4de5e8737181744
BottyIvan <droidbotty@gmail.com>
2025-09-01 23:27:23 +0200

    Reorganize resource loading code for improved clarity and error handling

--- cloud/ivanbotty/Launcher/__main__.py
+++ cloud/ivanbotty/Launcher/__main__.py
@@ -6,12 +6,12 @@ from gi.repository import Gio
 from cloud.ivanbotty.Launcher.app import App
 
 def main():
-    # try:
-    #     resource = Gio.Resource.load("cloud/ivanbotty/Launcher/resources/resources.gresource")
-    #     Gio.resources_register(resource)
-    # except Exception as e:
-    #     print(f"Failed to load resources: {e}")
-    #     return
+    try:
+        resource = Gio.Resource.load("cloud/ivanbotty/Launcher/resources/resources.gresource")
+        Gio.resources_register(resource)
+    except Exception as e:
+        print(f"Failed to load resources: {e}")
+        return
 
     app = App()
     app.run()
